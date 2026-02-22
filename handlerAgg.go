d7262e822149a1ac2ec9c8068a2b5c2343cf03e3
BrainArchive <zekeflores.aer@gmail.com>
2026-02-22 16:24:18 +0800

    refactored agg. Now refreshes feeds every 5 seconds

--- handlerAgg.go
+++ handlerAgg.go
@@ -2,17 +2,56 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
+	"time"
 
+	"github.com/brainarchive/goblogaggregator/internal/database"
 	"github.com/brainarchive/goblogaggregator/internal/rss"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	rssFeed, err := rss.FetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
+
+	ticker := time.NewTicker(5 * time.Second)
+	for ; ; <-ticker.C {
+		err := scrapeFeeds(s)
+		if err != nil {
+			return err
+		}
+	}
+
+}
+
+func scrapeFeeds(s *state) error {
+	latestFeed, err := s.db.GetNextFeedToFetch(context.Background())
 	if err != nil {
 		return err
 	}
+	s.db.MarkFeedFetched(context.Background(), database.MarkFeedFetchedParams{
+		LastFetchedAt: sql.NullTime{
+			Time:  time.Now(),
+			Valid: true,
+		},
+		UpdatedAt: time.Now(),
+		ID:        latestFeed.ID,
+	})
 
+	url := latestFeed.Url
+	rssFeed, err := rss.FetchFeed(context.Background(), url)
+	if err != nil {
+		return err
+	}
 	fmt.Printf("%v", *rssFeed)
+	fmt.Println()
+	fmt.Println("====================")
+	fmt.Printf("%v\n", latestFeed.Name)
+	fmt.Printf("%v\n", latestFeed.Url)
+	fmt.Printf("%v\n", latestFeed.UserID)
+	user, err := s.db.GetUserFromId(context.Background(), latestFeed.UserID)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%v\n", user.Name)
+	fmt.Println("====================")
 	return nil
 }
--- internal/database/feeds.sql.go
+++ internal/database/feeds.sql.go
@@ -7,6 +7,7 @@ package database
 
 import (
 	"context"
+	"database/sql"
 	"time"
 
 	"github.com/google/uuid"
@@ -22,7 +23,7 @@ VALUES (
     $5,
     $6
 )
-RETURNING id, created_at, updated_at, name, url, user_id
+RETURNING id, created_at, updated_at, name, url, user_id, last_fetched_at
 `
 
 type CreateFeedParams struct {
@@ -51,12 +52,13 @@ func (q *Queries) CreateFeed(ctx context.Context, arg CreateFeedParams) (Feed, e
 		&i.Name,
 		&i.Url,
 		&i.UserID,
+		&i.LastFetchedAt,
 	)
 	return i, err
 }
 
 const feedFromID = `-- name: FeedFromID :one
-SELECT id, created_at, updated_at, name, url, user_id FROM feeds WHERE id = $1
+SELECT id, created_at, updated_at, name, url, user_id, last_fetched_at FROM feeds WHERE id = $1
 `
 
 func (q *Queries) FeedFromID(ctx context.Context, id uuid.UUID) (Feed, error) {
@@ -69,12 +71,13 @@ func (q *Queries) FeedFromID(ctx context.Context, id uuid.UUID) (Feed, error) {
 		&i.Name,
 		&i.Url,
 		&i.UserID,
+		&i.LastFetchedAt,
 	)
 	return i, err
 }
 
 const feedFromUrl = `-- name: FeedFromUrl :one
-SELECT id, created_at, updated_at, name, url, user_id FROM feeds WHERE url = $1
+SELECT id, created_at, updated_at, name, url, user_id, last_fetched_at FROM feeds WHERE url = $1
 `
 
 func (q *Queries) FeedFromUrl(ctx context.Context, url string) (Feed, error) {
@@ -87,6 +90,26 @@ func (q *Queries) FeedFromUrl(ctx context.Context, url string) (Feed, error) {
 		&i.Name,
 		&i.Url,
 		&i.UserID,
+		&i.LastFetchedAt,
+	)
+	return i, err
+}
+
+const getNextFeedToFetch = `-- name: GetNextFeedToFetch :one
+SELECT id, created_at, updated_at, name, url, user_id, last_fetched_at FROM feeds ORDER BY last_fetched_at NULLS FIRST LIMIT 1
+`
+
+func (q *Queries) GetNextFeedToFetch(ctx context.Context) (Feed, error) {
+	row := q.db.QueryRowContext(ctx, getNextFeedToFetch)
+	var i Feed
+	err := row.Scan(
+		&i.ID,
+		&i.CreatedAt,
+		&i.UpdatedAt,
+		&i.Name,
+		&i.Url,
+		&i.UserID,
+		&i.LastFetchedAt,
 	)
 	return i, err
 }
@@ -122,4 +145,21 @@ func (q *Queries) ListFeeds(ctx context.Context) ([]ListFeedsRow, error) {
 		return nil, err
 	}
 	return items, nil
+}
+
+const markFeedFetched = `-- name: MarkFeedFetched :exec
+UPDATE feeds 
+SET last_fetched_at = $1, updated_at = $2
+WHERE id = $3
+`
+
+type MarkFeedFetchedParams struct {
+	LastFetchedAt sql.NullTime
+	UpdatedAt     time.Time
+	ID            uuid.UUID
+}
+
+func (q *Queries) MarkFeedFetched(ctx context.Context, arg MarkFeedFetchedParams) error {
+	_, err := q.db.ExecContext(ctx, markFeedFetched, arg.LastFetchedAt, arg.UpdatedAt, arg.ID)
+	return err
 }
